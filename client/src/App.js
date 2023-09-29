3d5722d57fc8365732b7fa01bed748d338876ef5
Pasindu599 <pasindusankalpa599@gmail.com>
2023-09-29 10:31:18 +0530

    create login page

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,19 @@ import Menu from './components/Menu/SideDrawer';
 import EditProfile from './screens/ProfileEdit/index';
 import Header from './components/Header/index';
 import LeavingRequest from './screens/LeavingReport';
+
 function App() {
-  return <LeavingRequest />;
+  return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/home" element={<Header />} />
+    //     <Route path="/profile" element={<EditProfile />} />
+    //     <Route path="/leaving" element={<LeavingRequest />} />
+    //   </Routes>
+    // </Router>
+    <Login />
+  );
 }
 
 export default App;
