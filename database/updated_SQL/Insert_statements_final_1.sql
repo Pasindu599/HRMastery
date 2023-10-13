902458a45f4b661f5cb1d3c5fec04679455a3bfe
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2023-10-14 01:03:27 +0530

    Update Insert_statements_final_1.sql

--- database/updated_SQL/Insert_statements_final_1.sql
+++ database/updated_SQL/Insert_statements_final_1.sql
@@ -55,19 +55,45 @@ VALUES
 (4, 3, 20),
 (4, 4, 50);
 
+
+-- employees
+INSERT INTO `employees` (`first_name`, `last_name`, `gender`, `birthdate`,`marital_status`, `supervisor_id`, `department_id`, `pay_grade_id`, `employee_status_id`, `job_title_id`)
+VALUES
+('Chamari', 'Fernando', 'female', '1989-05-07', 1, 'A0002', 2, 3, 4, 2),
+('Dinuka', 'Perera', 'male', '1990-11-15', 0, 'A0001', 3, 1, 5, 1),
+('Isuri', 'Silva', 'female', '1986-04-20', 1, 'A0002', 1, 2, 2, 1),
+('Janith', 'Karunaratne', 'male', '1992-03-03', 0, 'A0002', 3, 4, 5, 2),
+('Nethmi', 'Ratnayake', 'female', '1993-08-18', 1, 'A0003', 1, 2, 1, 3),
+('Pasan', 'Jayawardena', 'male', '1987-12-29', 0, 'A0003', 2, 1, 5, 3),
+('Dimalsha', 'Wijeratne', 'female', '1988-07-09', 1, 'A0003', 3, 3, 2, 2),
+('Sahan', 'Bandara', 'male', '1985-01-26', 0, 'A0002', 1, 4, 4, 3),
+('Sanduni', 'Perera', 'female', '1989-10-03', 1, 'A0004', 2, 1, 3, 4),
+('Lahiru', 'Dissanayake', 'male', '1986-06-17', 0, 'A0003', 3, 2, 6, 1),
+('Chathuri', 'Gunasekara', 'female', '1990-03-22', 1, 'A0002', 1, 3, 1, 2),
+('Nisal', 'Samarasekera', 'male', '1991-09-11', 0, 'A0003', 2, 4, 2, 3),
+('Shanika', 'Mendis', 'female', '1992-04-07', 1, 'A0004', 3, 1, 5, 4),
+('Sankalpa', 'Wickramasinghe', 'male', '1988-08-16', 0, 'A0004', 1, 2, 6, 1),
+('Thisari', 'Jayasundara', 'female', '1987-11-24', 1, 'A0003', 2, 3, 3, 2),
+('Ashen', 'Senanayake', 'male', '1993-02-12', 0, 'A0004', 3, 4, 4, 3),
+('Kavindya', 'Alwis', 'female', '1986-10-28', 1, 'A0003', 1, 1, 2, 4),
+('Milinda', 'Rajapaksa', 'male', '1985-05-05', 0, 'A0003', 2, 2, 1, 1),
+('Ruvini', 'Peris', 'female', '1989-09-09', 1, 'A0004', 3, 3, 5, 2),
+('Buddhika', 'Kodikara', 'male', '1990-07-15', 0, 'A0003', 1, 4, 6, 3),
+('Dinuka', 'Perera', 'male', '1980-11-15', 0, 'A0003', 3, 1, 4, 1);
+
 -- user accounts
 INSERT INTO `HRM`.`user_accounts` (`username`, `password`, `user_email`, `employee_id`, `role_id`)
 VALUES 
 ('ChamariFernando', 'password123', 'chamari.fernando@gmail.com', 'A0001', 1),
 ('DinukaPerera', 'password123', 'dinuka.perera@gmail.com', 'A0002', 2),
 ('IsuriSilva', 'password123', 'isuri.silva@gmail.com', 'A0003', 3),
-('JanithKarunaratne', 'password123', 'janith.karunaratne@gmail.com', 'A0004', 1),
-('NethmiRatnayake', 'password123', 'nethmi.ratnayake@gmail.com', 'A0005', 2),
+('JanithKarunaratne', 'password123', 'janith.karunaratne@gmail.com', 'A0004', 3),
+('NethmiRatnayake', 'password123', 'nethmi.ratnayake@gmail.com', 'A0005', 3),
 ('PasanJayawardena', 'password123', 'pasan.jayawardena@gmail.com', 'A0006', 3),
-('DimalshaWijeratne', 'password123', 'dimalsha.wijeratne@gmail.com', 'A0007', 1),
-('SahanBandara', 'password123', 'sahan.bandara@gmail.com', 'A0008', 2),
+('DimalshaWijeratne', 'password123', 'dimalsha.wijeratne@gmail.com', 'A0007', 3),
+('SahanBandara', 'password123', 'sahan.bandara@gmail.com', 'A0008', 3),
 ('SanduniPerera', 'password123', 'sanduni.perera@gmail.com', 'A0009', 3),
-('LahiruDissanayake', 'password123', 'lahiru.dissanayake@gmail.com', 'A0010', 1);
+('LahiruDissanayake', 'password123', 'lahiru.dissanayake@gmail.com', 'A0010', 3);
 
 -- emergency contact details
 INSERT INTO `HRM`.`emergency_contact_details` (`contact_name`, `relationship`, `contact_number`, `employee_id`)
@@ -93,45 +119,20 @@ VALUES
 ('Kumudini Peris', 'Aunty', '+94 701234579', 'A0019'),
 ('Charith Kodikara', 'Uncle', '+94 701234580', 'A0020');
 
--- employees
-INSERT INTO `employees` (`first_name`, `last_name`, `gender`, `birthdate`,`marital_status`, `supervisor_id`, `department_id`, `pay_grade_id`, `employee_status_id`, `job_title_id`)
-VALUES
-('Chamari', 'Fernando', 'female', '1989-05-07', 1, 4, 2, 3, 4, 2),
-('Dinuka', 'Perera', 'male', '1990-11-15', 0, NULL, 3, 1, 5, 1),
-('Isuri', 'Silva', 'female', '1986-04-20', 1, 6, 1, 2, 2, 1),
-('Janith', 'Karunaratne', 'male', '1992-03-03', 0, 7, 3, 4, 5, 2),
-('Nethmi', 'Ratnayake', 'female', '1993-08-18', 1, 2, 1, 2, 1, 3),
-('Pasan', 'Jayawardena', 'male', '1987-12-29', 0, NULL, 2, 1, 5, 3),
-('Dimalsha', 'Wijeratne', 'female', '1988-07-09', 1, 9, 3, 3, 2, 2),
-('Sahan', 'Bandara', 'male', '1985-01-26', 0, 10, 1, 4, 4, 3),
-('Sanduni', 'Perera', 'female', '1989-10-03', 1, 1, 2, 1, 3, 4),
-('Lahiru', 'Dissanayake', 'male', '1986-06-17', 0, 3, 3, 2, 6, 1),
-('Chathuri', 'Gunasekara', 'female', '1990-03-22', 1, 4, 1, 3, 1, 2),
-('Nisal', 'Samarasekera', 'male', '1991-09-11', 0, 5, 2, 4, 2, 3),
-('Shanika', 'Mendis', 'female', '1992-04-07', 1, 6, 3, 1, 5, 4),
-('Sankalpa', 'Wickramasinghe', 'male', '1988-08-16', 0, 7, 1, 2, 6, 1),
-('Thisari', 'Jayasundara', 'female', '1987-11-24', 1, 2, 2, 3, 3, 2),
-('Ashen', 'Senanayake', 'male', '1993-02-12', 0, 8, 3, 4, 4, 3),
-('Kavindya', 'Alwis', 'female', '1986-10-28', 1, 9, 1, 1, 2, 4),
-('Milinda', 'Rajapaksa', 'male', '1985-05-05', 0, 1, 2, 2, 1, 1),
-('Ruvini', 'Peris', 'female', '1989-09-09', 1, 3, 3, 3, 5, 2),
-('Buddhika', 'Kodikara', 'male', '1990-07-15', 0, 2, 1, 4, 6, 3),
-('Dinuka', 'Perera', 'male', '1980-11-15', 0, 14, 3, 1, 4, 1);
-
 -- leave requests
 INSERT INTO `HRM`.`leave_requests` 
-(`request_id`, `reason`, `leave_day_count`, `request_date`, `approved`, `employee_id`, `leave_type_id`) 
+(`reason`, `leave_day_count`, `request_date`, `approved`, `employee_id`, `leave_type_id`) 
 VALUES 
-(1, 'Family event', 3, '2023-01-01 09:00:00', 1, 'A0001', 1),
-(2, 'Medical leave', 2, '2023-01-05 10:30:00', 0, 'A0002', 2),
-(3, 'Vacation', 5, '2023-01-10 14:15:00', 1, 'A0003', 3),
-(4, 'Maternity leave', 60, '2023-01-20 16:20:00', 1, 'A0019', 3),
-(5, 'Family event', 1, '2023-01-25 11:05:00', 0, 'A0005', 1),
-(6, 'Medical leave', 4, '2023-01-30 12:10:00', 1, 'A0006', 2),
-(7, 'Vacation', 7, '2023-02-05 13:30:00', 0, 'A0007', 3),
-(8, 'Medical leave', 3, '2023-02-10 10:00:00', 1, 'A0008', 2),
-(9, 'Family event', 2, '2023-02-15 15:40:00', 0, 'A0009', 1),
-(10, 'Vacation', 10, '2023-02-20 11:50:00', 1, 'A0010', 3);
+('Family event', 3, '2023-01-01 09:00:00', 1, 'A0001', 1),
+('Medical leave', 2, '2023-01-05 10:30:00', 0, 'A0002', 2),
+('Vacation', 5, '2023-01-10 14:15:00', 1, 'A0003', 3),
+('Maternity leave', 60, '2023-01-20 16:20:00', 1, 'A0019', 3),
+('Family event', 1, '2023-01-25 11:05:00', 0, 'A0005', 1),
+('Medical leave', 4, '2023-01-30 12:10:00', 1, 'A0006', 2),
+('Vacation', 7, '2023-02-05 13:30:00', 0, 'A0007', 3),
+('Medical leave', 3, '2023-02-10 10:00:00', 1, 'A0008', 2),
+('Family event', 2, '2023-02-15 15:40:00', 0, 'A0009', 1),
+('Vacation', 10, '2023-02-20 11:50:00', 1, 'A0010', 3);
 
 -- dependents
 INSERT INTO `HRM`.`dependents` (`employee_id`, `dependent_name`, `relationship`, `age`) VALUES
