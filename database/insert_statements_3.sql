77897483c441e7402b186615737a4907522d680d
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2023-10-18 02:36:47 +0530

    Update insert_statements_3.sql

--- database/insert_statements_3.sql
+++ database/insert_statements_3.sql
@@ -60,10 +60,10 @@ VALUES
 INSERT INTO `employees` (`first_name`, `last_name`, `gender`, `birthdate`,`marital_status`, `supervisor_id`, `department_id`, `pay_grade_id`, `employee_status_id`, `job_title_id`)
 VALUES
 ('Chamari', 'Fernando', 'female', '1989-05-07', 1, 'A0002', 2, 3, 4, 2),
-('Dinuka', 'Perera', 'male', '1990-11-15', 0, 'A0003', 3, 1, 5, 1),
+('Dinuka', 'Perera', 'male', '1990-11-15', 0, 'A0001', 3, 1, 5, 1),
 ('Isuri', 'Silva', 'female', '1986-04-20', 1, 'A0002', 1, 2, 2, 1),
-('Janith', 'Karunaratne', 'male', '1992-03-03', 0, 'A0003', 3, 4, 5, 2),
-('Nethmi', 'Ratnayake', 'female', '1993-08-18', 1, 'A0003', 1, 2, 1, 3),
+('Janith', 'Karunaratne', 'male', '1992-03-03', 0, 'A0002', 3, 4, 5, 2),
+('Nethmi', 'Ratnayake', 'female', '1993-08-18', 1, 'A0002', 1, 2, 1, 3),
 ('Pasan', 'Jayawardena', 'male', '1987-12-29', 0, 'A0003', 2, 1, 5, 3),
 ('Dimalsha', 'Wijeratne', 'female', '1988-07-09', 1, 'A0003', 3, 3, 2, 2),
 ('Sahan', 'Bandara', 'male', '1985-01-26', 0, 'A0002', 1, 4, 4, 3),
@@ -127,13 +127,12 @@ VALUES
 ('Family event', 3, '2023-01-01', 1, 'A0001', 1),
 ('Medical leave', 2, '2023-01-05', 0, 'A0002', 2),
 ('Vacation', 5, '2023-01-10', 1, 'A0003', 2),
-('Maternity leave', 60, '2023-01-20', 1, 'A0019', 3),
 ('Family event', 1, '2023-01-25', 0, 'A0005', 1),
 ('Medical leave', 4, '2023-01-30', 1, 'A0006', 2),
 ('Vacation', 7, '2023-02-05', 0, 'A0007', 2),
 ('Medical leave', 3, '2023-02-10', 1, 'A0008', 2),
-('Family event', 2, '2023-02-15', 0, 'A0009', 1),
-('Vacation', 10, '2023-02-20', 1, 'A0010', 2);
+('Family event', 2, '2023-02-15', 0, 'A0009', 1);
+
 
 -- dependents
 INSERT INTO `HRM`.`dependents` (`employee_id`, `dependent_name`, `relationship`, `age`) VALUES
